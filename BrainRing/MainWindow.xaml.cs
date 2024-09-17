2fe4d1e1460041a0f9b32cba6093e516fb1d8ae7
Nikolay Kolgotin <nikolay.kolgotin@rt.ru>
2024-09-17 14:29:24 +0300

    подключен логер;
    добавлена запись в лог при вылете необработанной ошибке

--- BrainRing/MainWindow.xaml.cs
+++ BrainRing/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using System.Windows;
-using BrainRing.UI;
 
 namespace BrainRing;
 
--- BrainRing/MainWindowViewModel.cs
+++ BrainRing/MainWindowViewModel.cs
@@ -1,16 +1,25 @@
-﻿using BrainRing.UI;
+﻿using System;
+using BrainRing.UI;
 using DynamicData.Binding;
 using System.Windows;
+using NLog;
 
 namespace BrainRing;
 
 public class MainWindowViewModel : AbstractNotifyPropertyChanged
 {
+    private readonly Logger _logger;
+
     public MainWindowViewModel()
     {
+        AppDomain currentDomain = AppDomain.CurrentDomain;
+        currentDomain.UnhandledException += MyHandler;
+
+        _logger = LogManager.GetCurrentClassLogger();
+        _logger.Info("Приложение стартовало");
         var props = Application.Current.Properties;
-        if (props.Contains(App.MODE_NAME)
-            && props[App.MODE_NAME] is App.EDIT_NAME)
+
+        if (props.Contains(App.MODE_NAME) && props[App.MODE_NAME] is App.EDIT_NAME)
         {
             MainContent = new MainEditViewModel();
         }
@@ -21,4 +30,9 @@ public class MainWindowViewModel : AbstractNotifyPropertyChanged
     }
 
     public object MainContent { get; }
+
+    private void MyHandler(object sender, UnhandledExceptionEventArgs args)
+    {
+        _logger.Error(args.ExceptionObject);
+    }
 }
